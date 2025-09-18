83926b1d5adfa2b7b4ba06eaa6abb16805cdc2ed
Dario <dario.guarracino2@gmail.com>
2025-09-18 15:12:33 +0100

    Update platform_sdl.h to show Key mapping

--- platform_sdl.h
+++ platform_sdl.h
@@ -9,6 +9,7 @@
 
 // Mimic the classic Keyboard layout
 // todo: Configurable?
+// Maps the Key on the keyboard to the corresponding CHIP8 Key: see comment below for mapping visualisation.
 const static uint8_t KEYMAP[NUM_KEYS] = {
 	SDLK_X, SDLK_1, SDLK_2, SDLK_3, // 0, 1, 2, 3
 	SDLK_Q, SDLK_W, SDLK_E, SDLK_A, // 4, 5, 6, 7,
@@ -16,10 +17,10 @@ const static uint8_t KEYMAP[NUM_KEYS] = {
 	SDLK_4, SDLK_R, SDLK_F, SDLK_V, // C, D, E, F
 };
 // Look at your these keys on your keyboard: this is your CHIP-8 keypad 
-// 1, 2, 3, C
-// 4, 5, 6, D
-// 7, 8, 9, E,
-// A, 0, B, F
+// 1, 2, 3, 4    			1, 2, 3, C
+// Q, W, E, R	  maps to 	4, 5, 6, D
+// A, S, D, F	   ---> 	7, 8, 9, E,
+// Z, X, C, V				A, 0, B, F
 typedef struct SDLPlatform {
 	struct Platform base;
 	uint8_t running;
